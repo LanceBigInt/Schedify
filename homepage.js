79ca62f00f5837e12e48d5ef0cf7d876558f9e14
Lance <lance.algabre@outlook.com>
2024-11-30 14:12:46 +0800

    Added interactivity to choosing files.

--- homepage.js
+++ homepage.js
@@ -1,3 +1,17 @@
 const button = document.getElementById("gen-button");
 const fileInput = document.getElementById("choose-file");
 
+// Initially hide the button
+button.style.visibility = "hidden";
+
+// Main logic of the app.
+fileInput.addEventListener("change", (buttonhide) => {
+    if (fileInput.files.length === 0) {
+        button.style.visibility = "hidden";
+    } else {
+        button.style.visibility = "visible";
+    }
+});
+
+
+
